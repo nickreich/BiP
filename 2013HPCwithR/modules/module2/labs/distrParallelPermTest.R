8815f4ccbb9733669c149714cbbc65f4626e0445
Nicholas Reich <nick@schoolph.umass.edu>
2014-02-03 06:56:19 -0500

    distributed script writing to external file with cat()

--- 2013HPCwithR/modules/module2/labs/distrParallelPermTest.R
+++ 2013HPCwithR/modules/module2/labs/distrParallelPermTest.R
@@ -9,16 +9,20 @@ nCores <- 10
 
 registerDoMC(nCores)
 
-#setwd("/Users/nick/Documents/code_versioned/BiP/2013HPCwithR/modules/module2/labs/")
-setwd("/home/ngr67a/BiP/")
+setwd("/Users/nick/Documents/code_versioned/BiP/2013HPCwithR/modules/module2/labs/")
+#setwd("/home/ngr67a/BiP/")
 hts <- read.csv("heights.csv")
 
 ## run permutation loop, storing each time
 mat <- foreach(i=1:nSim, .combine=rbind) %dopar% {
         permDhts <- sample(hts$Dheight, replace=FALSE)
         mdl <- lm(permDhts ~ hts$Mheight)
-        c(i, coef(mdl))
+        output <- c(i, coef(mdl))
+        lineText <- paste(round(output,4), collapse=",")
+        cat(c(lineText, "\n"), file="coefsDistr.csv", append=TRUE)
+        output
 }
 colnames(mat) <- c("iter", "b0", "b1")
 
-write.csv(mat, file="coefsDistr.csv", row.names=FALSE, append=TRUE)
+## notes that lines in coefDistr.csv are not in order
+## rows in mat object are
