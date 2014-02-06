66b0b3e7aa6f88c1f267c44f2da8be7439febdd3
Nicholas Reich <nick@schoolph.umass.edu>
2014-02-05 22:39:15 -0500

    updating foreach permtest and mod2 slides.

--- 2013HPCwithR/modules/module2/labs/permTest_foreach.R
+++ 2013HPCwithR/modules/module2/labs/permTest_foreach.R
@@ -19,16 +19,20 @@ hts <- read.csv("heights.csv")
 
 ## fit initial model and create storage file
 realDataModel <- lm(Dheight ~ Mheight, data=hts)
-lineOne <- c(0, coef(realDataModel))
+realData_beta1 <- coef(realDataModel)[2]
 
-## run permutation loop, storing each time
-matPar <- foreach(i=1:nSim, .combine=rbind) %dopar% {
+## run permutation loop
+tic <- Sys.time()
+mat <- foreach(i=1:nSim, .combine=rbind) %dopar% {
         permDhts <- sample(hts$Dheight, replace=FALSE)
         mdl <- lm(permDhts ~ hts$Mheight)
         c(i, coef(mdl))
 }
+toc <- Sys.time()
+(toc-tic)
 
-mat <- rbind(lineOne, matPar)
 colnames(mat) <- c("iter", "b0", "b1")
+(pval <- sum(abs(mat[,"b1"]) > realData_beta1)/nrow(mat))
 
-write.csv(mat, file="coefs.csv", row.names=FALSE)
+require(parallel)
+detectCores()
